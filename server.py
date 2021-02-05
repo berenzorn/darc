41133a4fcee117c416ea3aa53394ab487726e942
berenzorn <berenzorn@gmail.com>
2021-02-05 12:05:37 +0300

    multithreaded chunker

--- server.py
+++ server.py
@@ -1,14 +1,48 @@
+import threading
+from sys import argv
 from tasks import compress
+from queue import Queue
 
-with open('test2.lzma', 'w') as f:
-    f.truncate(0)
+# with open('test.lzma', 'w') as f:
+#     f.truncate(0)
 
-with open('test2.lzma', 'ab') as file2:
-    with open('test', 'rb') as file:
-        while True:
-            chunk = file.read(2560000)
-            if not chunk:
-                break
-            res = compress.delay(chunk)
-            file2.write(res.get()[1])
-            print('.', end='')
+# with open('test2.lzma', 'ab') as file2:
+#     with open('test', 'rb') as file:
+#         while True:
+#             chunk = file.read(25600000)
+#             if not chunk:
+#                 break
+#             res = compress.delay(chunk)
+#             file2.write(res.get(timeout=30))
+#             print('.', end='')
+
+q = Queue(maxsize=128)
+file2 = open(f'{argv[1]}.lzma', 'ab')
+
+
+def worker():
+    while True:
+        chunk = q.get()
+        res = compress.delay(chunk)
+        file2.write(res.get())
+        q.task_done()
+
+
+threads = 8
+for _ in range(threads):
+    threading.Thread(target=worker, daemon=True).start()
+
+with open(argv[1], 'rb') as file:
+    while True:
+        chunk = file.read(2560000)
+        if not chunk:
+            break
+        q.put(chunk)
+q.join()
+file2.close()
+
+# with open('test2.lzma', 'ab') as file2:
+#         res = compress.delay(chunk)
+#         file2.write(res.get(timeout=30))
+#         print('.', end='')
+
--- tasks.py
+++ tasks.py
@@ -5,11 +5,13 @@ app = Celery('tasks')
 app.conf.task_serializer = 'msgpack'
 app.conf.result_serializer = 'msgpack'
 app.conf.accept_content = ['json', 'msgpack']
-app.conf.broker_url = 'redis://localhost:6379/0'
-app.conf.result_backend = 'redis://localhost:6379/0'
+app.conf.broker_url = 'amqp://'
+app.conf.result_backend = 'rpc://'
+# app.conf.broker_url = 'redis://localhost:6379/0'
+# app.conf.result_backend = 'redis://localhost:6379/0'
 
 
 @app.task
 def compress(chunk):
-    return lzma.compress(chunk)
+    return lzma.compress(chunk, preset=lzma.PRESET_EXTREME)
 
